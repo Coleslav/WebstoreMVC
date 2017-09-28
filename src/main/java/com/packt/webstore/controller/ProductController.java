b7770a8902fbd42ce119a42e597e93c1db83a407
Kolin Krzysztof <kolinkrzysztof@gmail.com>
2017-09-28 09:58:31 +0200

    Ep III -Improved few imports -?productId= -Filters

--- src/main/java/com/packt/webstore/controller/ProductController.java
+++ src/main/java/com/packt/webstore/controller/ProductController.java
@@ -5,8 +5,12 @@ import com.packt.webstore.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.MatrixVariable;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.*;
 
 
 @Controller
@@ -33,5 +37,17 @@ public class ProductController {
     public String getProductsByCategory(Model model, @PathVariable("category")String productCategory){
         model.addAttribute("products", productService.getProductsByCategory(productCategory));
         return "products";
+    }
+
+    @RequestMapping("/filter/{ByCriteria}")
+    public String getProductsByFilter(@MatrixVariable(pathVar = "ByCriteria")Map<String,List<String>> filterParams, Model model){
+        model.addAttribute("products", productService.getProductsByFilter(filterParams));
+        return "products";
+    }
+
+    @RequestMapping("/product")
+    public String getProductById(@RequestParam("id") String productId, Model model) {
+        model.addAttribute("product", productService.getProductById(productId));
+        return "product";
     }
 }
--- src/main/java/com/packt/webstore/domain/repository/ProductRepository.java
+++ src/main/java/com/packt/webstore/domain/repository/ProductRepository.java
@@ -1,10 +1,12 @@
 package com.packt.webstore.domain.repository;
 
-import java.util.List;
+import java.util.*;
+
 import com.packt.webstore.domain.Product;
 
 public interface ProductRepository {
     List <Product> getAllProducts();
     Product getProductById(String productId);
     List<Product> getProductsByCategory(String category);
+    Set<Product> getProductsByFilter(Map<String, List<String >> filterParams);
 }
